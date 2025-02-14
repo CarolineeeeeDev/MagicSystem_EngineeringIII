35b07650cb1bdd5f9594247b11b38dd5ea65f525
u1473700 <LilScorpionC@gmail.com>
2025-02-14 05:26:53 -0700

    add replicated

--- MagicSystem/Source/MagicSystem/MagicSystemAttributeSet.cpp
+++ MagicSystem/Source/MagicSystem/MagicSystemAttributeSet.cpp
@@ -1,2 +1,75 @@
 #include "MagicSystemAttributeSet.h"
+#include "GameplayEffectExtension.h"
+#include "Net/UnrealNetwork.h"
 
+UMagicSystemAttributeSet::UMagicSystemAttributeSet()
+{
+
+}
+
+void UMagicSystemAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME_CONDITION_NOTIFY(UMagicSystemAttributeSet, currentHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UMagicSystemAttributeSet, maxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UMagicSystemAttributeSet, currentMana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UMagicSystemAttributeSet, maxMana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UMagicSystemAttributeSet, attackDamage, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UMagicSystemAttributeSet, speedMultiplier, COND_None, REPNOTIFY_Always);
+}
+
+void UMagicSystemAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+	if (Data.EvaluatedData.Attribute == GetcurrentHealthAttribute())
+	{
+		SetcurrentHealth(FMath::Clamp(GetcurrentHealth(), 0.0f, GetmaxHealth()));
+		HealthChangeDelegate.Broadcast(GetcurrentHealth(), Data.EffectSpec.StackCount);
+	}
+	if (Data.EvaluatedData.Attribute == GetcurrentManaAttribute())
+	{
+		SetcurrentMana(FMath::Clamp(GetcurrentMana(), 0.0f, GetmaxMana()));
+		ManaChangeDelegate.Broadcast(GetcurrentMana(), Data.EffectSpec.StackCount);
+	}
+	if (Data.EvaluatedData.Attribute == GetattackDamageAttribute())
+	{
+		SetattackDamage(FMath::Clamp(GetattackDamage(), 0.0f, 100.0f));
+		AttackDamageChangeDelegate.Broadcast(GetattackDamage(), Data.EffectSpec.StackCount);
+	}
+	if (Data.EvaluatedData.Attribute == GetspeedMultiplierAttribute())
+	{
+		SetspeedMultiplier(FMath::Clamp(GetspeedMultiplier(), 0.1f, 2.0f));
+		SpeedMultiplierChangeDelegate.Broadcast(GetspeedMultiplier(), Data.EffectSpec.StackCount);
+	}
+}
+
+void UMagicSystemAttributeSet::OnRep_currentHealth(const FGameplayAttributeData& oldCurrentHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMagicSystemAttributeSet, currentHealth, oldCurrentHealth);
+
+}
+
+void UMagicSystemAttributeSet::OnRep_maxHealth(const FGameplayAttributeData& oldMaxHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMagicSystemAttributeSet, maxHealth, oldMaxHealth);
+}
+
+void UMagicSystemAttributeSet::OnRep_currentMana(const FGameplayAttributeData& oldCurrentMana)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMagicSystemAttributeSet, currentMana, oldCurrentMana);
+}
+
+void UMagicSystemAttributeSet::OnRep_maxMana(const FGameplayAttributeData& oldMaxMana)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMagicSystemAttributeSet, maxMana, oldMaxMana);
+}
+
+void UMagicSystemAttributeSet::OnRep_attackDamage(const FGameplayAttributeData& oldAttackDamage)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMagicSystemAttributeSet, attackDamage, oldAttackDamage);
+}
+
+void UMagicSystemAttributeSet::OnRep_speedMultiplier(const FGameplayAttributeData& oldSpeedMultiplier)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMagicSystemAttributeSet, speedMultiplier, oldSpeedMultiplier);
+}
--- MagicSystem/Source/MagicSystem/MagicSystemAttributeSet.h
+++ MagicSystem/Source/MagicSystem/MagicSystemAttributeSet.h
@@ -2,7 +2,16 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "GameplayEffect.h"
+#include "MagicSystemAbilityComponent.h"
 #include "MagicSystemAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
 
 struct FGameplayEffectModCallbackData;
 
@@ -16,8 +25,50 @@ class MAGICSYSTEM_API UMagicSystemAttributeSet : public UAttributeSet
 public:
 	UMagicSystemAttributeSet();
 
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Attribute", ReplicatedUsing = OnRep_currentHealth)
+	FGameplayAttributeData currentHealth;
+	ATTRIBUTE_ACCESSORS(UMagicSystemAttributeSet, currentHealth);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attribute", ReplicatedUsing = OnRep_maxHealth)
+	FGameplayAttributeData maxHealth;
+	ATTRIBUTE_ACCESSORS(UMagicSystemAttributeSet, maxHealth);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attribute", ReplicatedUsing = OnRep_currentMana)
+	FGameplayAttributeData currentMana;
+	ATTRIBUTE_ACCESSORS(UMagicSystemAttributeSet, currentMana);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attribute", ReplicatedUsing = OnRep_maxMana)
+	FGameplayAttributeData maxMana;
+	ATTRIBUTE_ACCESSORS(UMagicSystemAttributeSet, maxMana);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attribute", ReplicatedUsing = OnRep_attackDamage)
+	FGameplayAttributeData attackDamage;
+	ATTRIBUTE_ACCESSORS(UMagicSystemAttributeSet, attackDamage);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attribute", ReplicatedUsing = OnRep_speedMultiplier)
+	FGameplayAttributeData speedMultiplier;
+	ATTRIBUTE_ACCESSORS(UMagicSystemAttributeSet, speedMultiplier);
+
 	FAttrChangeDelegate HealthChangeDelegate;
 	FAttrChangeDelegate ManaChangeDelegate;
 	FAttrChangeDelegate AttackDamageChangeDelegate;
 	FAttrChangeDelegate SpeedMultiplierChangeDelegate;
+
+	void PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data) override;
+	
+	UFUNCTION()
+	virtual void OnRep_currentHealth(const FGameplayAttributeData& oldCurrentHealth);
+	UFUNCTION()
+	virtual void OnRep_maxHealth(const FGameplayAttributeData& oldMaxHealth);
+	UFUNCTION()
+	virtual void OnRep_currentMana(const FGameplayAttributeData& oldCurrentMana);
+	UFUNCTION()
+	virtual void OnRep_maxMana(const FGameplayAttributeData& oldMaxMana);
+	UFUNCTION()
+	virtual void OnRep_attackDamage(const FGameplayAttributeData& oldAttackDamage);
+	UFUNCTION()
+	virtual void OnRep_speedMultiplier(const FGameplayAttributeData& oldSpeedMultiplier);
 };
