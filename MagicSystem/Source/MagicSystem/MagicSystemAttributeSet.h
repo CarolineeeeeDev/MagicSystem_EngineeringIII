e61dc5e9b4586d30a053537b8011c65dff008e94
u1473700 <LilScorpionC@gmail.com>
2025-02-14 03:26:02 -0700

    create delegate

--- MagicSystem/Source/MagicSystem/MagicSystemAttributeSet.h
+++ MagicSystem/Source/MagicSystem/MagicSystemAttributeSet.h
@@ -4,9 +4,20 @@
 #include "AttributeSet.h"
 #include "MagicSystemAttributeSet.generated.h"
 
+struct FGameplayEffectModCallbackData;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FAttrChangeDelegate, float, Attr, int32, stackCount);
+
 UCLASS()
 class MAGICSYSTEM_API UMagicSystemAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
 	
+public:
+	UMagicSystemAttributeSet();
+
+	FAttrChangeDelegate HealthChangeDelegate;
+	FAttrChangeDelegate ManaChangeDelegate;
+	FAttrChangeDelegate AttackDamageChangeDelegate;
+	FAttrChangeDelegate SpeedMultiplierChangeDelegate;
 };
--- MagicSystem/Source/MagicSystem/MainCharacter.cpp
+++ MagicSystem/Source/MagicSystem/MainCharacter.cpp
@@ -1,31 +1,64 @@
 #include "MainCharacter.h"
+#include "MagicSystemAbilityComponent.h"
+#include "MagicSystemAttributeSet.h"
 
-// Sets default values
 AMainCharacter::AMainCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	AbilitySystemComponent = CreateDefaultSubobject<UMagicSystemAbilityComponent>("ACS Comp");
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
 }
 
-// Called when the game starts or when spawned
 void AMainCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	if (AbilitySystemComponent)
+	{
+		AttributeSet = AbilitySystemComponent->GetSet<UMagicSystemAttributeSet>();
+		const_cast<UMagicSystemAttributeSet*>(AttributeSet)->HealthChangeDelegate.AddDynamic(this, AMainCharacter::OnHealthChangedNative);
+		const_cast<UMagicSystemAttributeSet*>(AttributeSet)->ManaChangeDelegate.AddDynamic(this, AMainCharacter::OnManaChangedNative);
+		const_cast<UMagicSystemAttributeSet*>(AttributeSet)->AttackDamageChangeDelegate.AddDynamic(this, AMainCharacter::OnAttackDamageChangedNative);
+		const_cast<UMagicSystemAttributeSet*>(AttributeSet)->SpeedMultiplierChangeDelegate.AddDynamic(this, AMainCharacter::OnSpeedMultiplierChangedNative);
+		
+	}
 }
 
-// Called every frame
 void AMainCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
-// Called to bind functionality to input
 void AMainCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+}
+
+UAbilitySystemComponent* AMainCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+void AMainCharacter::OnHealthChangedNative(float health, int32 staticCounut)
+{
+	OnHealthChanged(health, staticCounut);
+}
+
+void AMainCharacter::OnManaChangedNative(float mana, int32 staticCounut)
+{
+	OnManaChanged(mana, staticCounut);
+}
+
+void AMainCharacter::OnAttackDamageChangedNative(float attackDamage, int32 staticCounut)
+{
+	OnAttackDamageChanged(attackDamage, staticCounut);
+}
+
+void AMainCharacter::OnSpeedMultiplierChangedNative(float speedMultiplier, int32 staticCounut)
+{
+	OnSpeedMultiplierChanged(speedMultiplier, staticCounut);
 }
 
