22d6ebf4d34a2d1e9ecfc409274214fc3df58d19
u1473700 <LilScorpionC@gmail.com>
2025-02-15 02:25:45 -0700

    slow ability

---
+++ MagicSystem/Source/MagicSystem/GameplayAbilitySlow.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MagicSystemGameplayAbility.h"
+#include "GameplayAbilitySlow.generated.h"
+
+UCLASS()
+class MAGICSYSTEM_API UGameplayAbilitySlow : public UMagicSystemGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
---
+++ MagicSystem/Source/MagicSystem/GameplayEffectSlow.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "GameplayEffectSlow.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAGICSYSTEM_API UGameplayEffectSlow : public UGameplayEffect
+{
+	GENERATED_BODY()
+	
+};
