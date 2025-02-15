9d8e79939df7f6d1ae813f2f832213aa08abc681
u1473700 <LilScorpionC@gmail.com>
2025-02-14 23:56:14 -0700

    ball ability

---
+++ MagicSystem/Source/MagicSystem/GameplayAbilityBall.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MagicSystemGameplayAbility.h"
+#include "GameplayAbilityBall.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAGICSYSTEM_API UGameplayAbilityBall : public UMagicSystemGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
---
+++ MagicSystem/Source/MagicSystem/GameplayEffectBall.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "GameplayEffectBall.generated.h"
+
+UCLASS()
+class MAGICSYSTEM_API UGameplayEffectBall : public UGameplayEffect
+{
+	GENERATED_BODY()
+	
+};
